9868b271d206dd0760d465b7c2c28e0d28730e93
030239230138-bnn <030239230138@st.buh.edu.vn>
2026-04-15 16:04:18 +0700

    Tìm kiếm và hiển thị logo

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-
+Route::get('/san-pham/{id}', [HomeController::class, 'show'])->name('sanpham.show');
 Route::get('/dashboard', function () {
     //return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -13,3 +13,6 @@ Route::get('/dashboard', function () {
 
 
 require __DIR__.'/auth.php';
+use App\Http\Controllers\SanPhamController;
+
+Route::post('/timkiem', [SanPhamController::class, 'search'])->name('search');
